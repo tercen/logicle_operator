2fb17578156e0a8eab738e46f9804f59b1a5a67c
emaurel <edgar.maurel@gmail.com>
2025-04-07 16:37:59 +0200

    fix : rotate output

--- main.R
+++ main.R
@@ -18,12 +18,13 @@ pars <- flowCore::estimateLogicle(ff, channels = 1:ncol(df))
 fs_trans <- transform(ff, pars)
 
 df_out <- exprs(fs_trans)
+df_out
 colnames(df_out) <- seq_len(ncol(df_out)) - 1L
 
 df_out %>% 
   as_tibble() %>%
-  mutate(.ci = seq_len(nrow(.)) - 1L) %>%
-  tidyr::pivot_longer(!matches(".ci"), names_to = ".ri") %>%
-  mutate(.ri = as.integer(.ri)) %>%
+  mutate(.ri = seq_len(nrow(.)) - 1L) %>%
+  tidyr::pivot_longer(!matches(".ri"), names_to = ".ci") %>%
+  mutate(.ci = as.integer(.ci)) %>%
   ctx$addNamespace() %>%
   ctx$save()
